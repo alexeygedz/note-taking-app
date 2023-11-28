c7f338041dddb81525731bf9fb99f3474e823a3c
Alexey Gedz <alexeygedz@gmail.com>
2023-11-28 17:10:40 +0500

    Правки

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			text: 'Горные походы открывают удивительные природные ландшафты',
+			date: new Date()
+		},
+		{
+			title: 'Поход в горы',
+			text: 'Думал, что очень много време...',
+			date: new Date()
+		}
+	];
 
 	return (
-        <>
-            <h1>Заголовок</h1>
-            <p>Какой-то текст</p>
-            <Button/>
-        </>
+		<>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button/>
+			<CardButton>
+                Новое воспоминание
+			</CardButton>
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
+			
+			<CardButton>
+				<JournalItem
+					title={data[1].title}
+					text={data[1].text}
+					date={data[1].date}
+				/>
+			</CardButton>
+		</>
 	);
 }
 
