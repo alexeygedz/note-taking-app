c46c6b88e50e0de39f3e262ab4d83190cf7da865
Alexey Gedz <alexeygedz@gmail.com>
2023-11-28 15:58:45 +0500

    Компонент Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
 
 	return (
         <>
-
+            <h1>Заголовок</h1>
+            <p>Какой-то текст</p>
+            <Button/>
         </>
 	);
 }
 
 export default App;
-
