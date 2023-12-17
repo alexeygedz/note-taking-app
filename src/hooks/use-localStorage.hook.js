1691c87f5769da1172c5cf48ebe864ab029499d5
Alexey Gedz <alexeygedz@gmail.com>
2023-12-17 11:42:59 +0500

    Правки localStorage

--- src/hooks/use-localStorage.hook.js
+++ src/hooks/use-localStorage.hook.js
@@ -3,6 +3,10 @@ import { useState, useEffect } from 'react';
 export function useLocalStorage(key) {
 	const [data, setData] = useState();
 
+	if (localStorage.getItem('data') === null) {
+		localStorage.setItem('data', JSON.stringify([]));
+	}
+
 	useEffect(() => {
 		const res = JSON.parse(localStorage.getItem(key));
 		if (res) {
